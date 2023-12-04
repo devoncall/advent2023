6428cf8e5290fd7daa8b683b0cb15faeccf5898c
Devon Call <dcall@liftoff.io>
2023-12-04 01:06:13 -0800

    Day 1 Puzzle 2 WIP

---
+++ day1puzzle2/src/main.rs
@@ -0,0 +1,47 @@
+use regex::Regex;
+use std::io;
+
+fn main() {
+    let mut total = 0;
+    for line in io::stdin().lines() {
+        total = total
+            + match line {
+                Ok(msg) => {
+                    let num = read_num(msg.clone());
+                    println!("{msg}: {num}");
+                    num
+                }
+                Err(_) => break,
+            }
+    }
+    println!("total: {total}")
+}
+
+fn read_num(s: String) -> u32 {
+    let find_num: Regex =
+        Regex::new(r"[0-9]|one|two|three|four|five|six|seven|eight|nine").unwrap();
+    let numbers: Vec<&str> = find_num.find_iter(&s).map(|m| m.as_str()).collect();
+    return match (numbers.first(), numbers.last()) {
+        (Some(first), Some(last)) => {
+            let f = to_digit(first);
+            let l = to_digit(last);
+            format!("{f}{l}").parse().unwrap()
+        }
+        _ => 0,
+    };
+}
+
+fn to_digit(s: &str) -> &str {
+    return match s {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => s,
+    };
+}
